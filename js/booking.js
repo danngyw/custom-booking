bdf68c0e7353b974d6330b4c934db841e7dd769f
Dan Ngyen <wpcodev@gmail.com>
2021-11-10 18:46:16 +0700

    upate js

--- js/booking.js
+++ js/booking.js
@@ -17,55 +17,13 @@
                     maxlength:14,
                     minlength:5
                 },
-                "re-password": {
-                    equalTo: "#password",
-                    minlength: 8
-                }
+                // "re-password": {
+                //     equalTo: "#password",
+                //     minlength: 8
+                // }
             },
             submitHandler: function(form) {
-                $(".booking-form").submit(function(event){
 
-                    event.preventDefault();
-                    var form    = $(event.currentTarget);
-                    if(! form.validate() ){
-                        return false;
-                    }
-
-                    var form    = $(event.currentTarget);
-                    var send    = {};
-                    form.find( 'input' ).each( function() {
-                        var key     = $(this).attr('name');
-                        send[key]   = $(this).val();
-                    });
-
-                    $.ajax({
-                        emulateJSON: true,
-                        url : booking.ajax_url,
-                        data: {
-                                action: 'save_booking',
-                                request: send,
-                        },
-                        beforeSend  : function(event){
-                            form.attr('disabled', 'disabled');
-                            form.find(".btn-submit").attr('disabled','disabled');
-                            //$(".btn-submit").
-                            form.find(".btn-submit").addClass("loading");
-                        },
-                        success : function(res){
-                            form.find(".btn-submit").removeAttr('disabled');
-                            form.find(".btn-submit").removeClass("loading");
-                            if ( res.success ){
-
-                                $(".booking-form").html('<h3>'+res.msg+'</h3>');
-                                alert(res.msg);
-                            } else {
-
-                            }
-                        }
-
-                    });
-                   return false;
-                });
             },
             onkeyup: function( element, event ) {
 
@@ -113,6 +71,47 @@
                     $('.button-submit').attr('disabled', true);
                 }
             }
+        });
+        $(".booking-form").submit(function(event){
+
+            event.preventDefault();
+            var form    = $(event.currentTarget);
+            if(! form.valid()){
+                return false;
+            }
+            var send    = {};
+            form.find( 'input' ).each( function() {
+                var key     = $(this).attr('name');
+                send[key]   = $(this).val();
+            });
+
+            $.ajax({
+                emulateJSON: true,
+                url : booking.ajax_url,
+                data: {
+                        action: 'save_booking',
+                        request: send,
+                },
+                beforeSend  : function(event){
+                    form.attr('disabled', 'disabled');
+                    form.find(".btn-submit").attr('disabled','disabled');
+                    //$(".btn-submit").
+                    form.find(".btn-submit").addClass("loading");
+                },
+                success : function(res){
+                    form.find(".btn-submit").removeAttr('disabled');
+                    form.find(".btn-submit").removeClass("loading");
+                    if ( res.success ){
+
+                        $(".booking-form").html('<h3>'+res.msg+'</h3>');
+                        alert(res.msg);
+                    } else {
+
+                    }
+                }
+
+            });
+           return false;
         });
 	});
 })(jQuery);
