e5139f0d303b24960879a7dc9331b13a134d1a09
Dan Ngyen <wpcodev@gmail.com>
2021-11-11 13:12:31 +0700

    update column

--- admin.php
+++ admin.php
@@ -26,48 +26,7 @@ function custom_menu() {
       11
     );
 }
-function booking_row_html($booking){?>
-  <tr id="post-8" class="iedit author-self level-0 post-8 type-room status-publish hentry rate-6 entry">
-      <th scope="row" class="check-column">     <label class="screen-reader-text" for="cb-select-8">
-        Select Phòng đơn      </label>
-      <input id="cb-select-8" type="checkbox" name="post[]" value="8">
-      <div class="locked-indicator">
-        <span class="locked-indicator-icon" aria-hidden="true"></span>
-        <span class="screen-reader-text">
-        “Phòng đơn” is locked       </span>
-      </div>
-      </th>
-      <td class="title column-title has-row-actions column-primary page-title" data-colname="Title">
-        <div class="locked-info"><span class="locked-avatar"></span> <span class="locked-text"></span></div>
-        <strong><a class="row-title" href="#" ><?php echo $booking->full_name;?></a></strong>
 
-<div class="hidden" id="inline_8">
-  <div class="post_title"><?php echo $booking->full_name;?></div><div class="post_name">phong-don</div>
-  <?php
-
-  $string = "?page=booking-room&action=delete&id={$booking->id}";
-  $trash_url = admin_url($string);
-  ?>
-  <div class="post_password"></div><div class="page_template">default</div>
-  <div class="tags_input" id="room_range_8"></div><div class="sticky"></div></div><div class="row-actions"><span class="edit"><a href="#" aria-label="Edit “Phòng đơn”">Edit</a> | </span><span class="inline hide-if-no-js">| </span>
-    <span class="trash"><a href="<?php echo $trash_url;?>" class="submitdelete" onclick="return confirm_delete()" >Delete</a> | </span>
-    <span class="view"><a href="#" rel="bookmark" aria-label="View ">View</a></span></div>
-  <td class="taxonomy-rate column-taxonomy-rate" data-colname="Rates"><?php echo $booking->phone;?> </td>
-  <td class="taxonomy-room_range column-taxonomy-room_range" data-colname="Range"><?php echo $booking->email;?></td>
-  <td class="date column-date" data-colname="Date">
-    <?php
-    $time = strtotime($booking->local_time);
-    if( $time>1 ) {
-        // echo 'Booked:<br>';
-        // //echo $booking->date_booking;
-        // echo date_i18n('Y/m/d h:i',$time , $gmt = true);
-        //echo get_the_date('Y/m/d', $time);
-        echo 'Booked:<br>'; echo date("Y/m/d H:i a", strtotime($booking->local_time));
-    };
-        ?>
-
-         </td>    </tr>
-  <?php }
 function html_list_booking(){
 
 
--- class-booking-admin.php
+++ class-booking-admin.php
@@ -4,6 +4,7 @@ Class bookingAdmin{
 	public $posts_per_page ;
 	public $search;
 	public $paged;
+	public $colums;
 	function __construct(){
 		$this->posts_per_page  = 10;
 		$this->search 	= isset($_GET['s']) ? $_GET['s'] : '';
@@ -16,11 +17,55 @@ Class bookingAdmin{
 	      $order = 'ASC';
 	      $this->order_url = admin_url('?page=booking-room');
 	    }
-		$colums = array('full_name','phone','email','local_date','status');
+		$this->colums = array(
+			'full_name' 	=> array('label'=>' Full name'),
+			'phone' 		=> array('label'=>'Phone'),
+			'email' 		=> array('label'=>'email'),
+			'local_date' 	=> array('label'=>'Date'),
+			'status' 		=> array('label'=>'Status'),
+		);
 	}
 
-	function item($booking){
+	function item($booking){ ?>
 
+  <tr id="post-8" class="iedit author-self level-0 post-8 type-room status-publish hentry rate-6 entry">
+      <th scope="row" class="check-column">     <label class="screen-reader-text" for="cb-select-8">
+        Select Phòng đơn      </label>
+      <input id="cb-select-8" type="checkbox" name="post[]" value="8">
+      <div class="locked-indicator">
+        <span class="locked-indicator-icon" aria-hidden="true"></span>
+        <span class="screen-reader-text">
+        “Phòng đơn” is locked       </span>
+      </div>
+      </th>
+      <td class="title column-title has-row-actions column-primary page-title" data-colname="Title">
+        <div class="locked-info"><span class="locked-avatar"></span> <span class="locked-text"></span></div>
+        <strong><a class="row-title" href="#" ><?php echo $booking->full_name;?></a></strong>
+
+<div class="hidden" id="inline_8">
+  <div class="post_title"><?php echo $booking->full_name;?></div><div class="post_name">phong-don</div>
+  <?php
+
+  $string = "?page=booking-room&action=delete&id={$booking->id}";
+  $trash_url = admin_url($string);
+  ?>
+  <div class="post_password"></div><div class="page_template">default</div>
+  <div class="tags_input" id="room_range_8"></div><div class="sticky"></div></div><div class="row-actions"><span class="edit"><a href="#" aria-label="Edit “Phòng đơn”">Edit</a> | </span><span class="inline hide-if-no-js">| </span>
+    <span class="trash"><a href="<?php echo $trash_url;?>" class="submitdelete" onclick="return confirm_delete()" >Delete</a> | </span>
+    <span class="view"><a href="#" rel="bookmark" aria-label="View ">View</a></span></div>
+  <td class="taxonomy-rate column-taxonomy-rate" data-colname="Rates"><?php echo $booking->phone;?> </td>
+  <td class="taxonomy-room_range column-taxonomy-room_range" data-colname="Range"><?php echo $booking->email;?></td>
+  <td class="date column-date" data-colname="Date">
+    <?php
+    $time = strtotime($booking->local_time);
+    if( $time>1 ) {
+        echo 'Booked:<br>'; echo date("Y/m/d H:i a", strtotime($booking->local_time));
+    };        ?>
+	</td>
+	<td class="date column-date" data-colname="Date">Status</td>
+
+</tr>
+         <?php
 	}
 	function show(){
 		$this->list_booking();
@@ -48,10 +93,6 @@ Class bookingAdmin{
 	        $search_where = " WHERE full_name LIKE '%{$this->search}%' OR email LIKE '%{$this->search}%' OR phone LIKE '%{$this->search}%'";
 	    }
 
-	    //page 1 - (records 01-10): offset = 0, limit=10;
-
-	    //page 2 - (records 11-20) offset = 10, limit =10;
-
 	    $sql = "SELECT * FROM $tbl_booking $search_where ORDER BY  id {$order}";
 
 	    $offset 			= ($this->paged-1)*$this->posts_per_page;
@@ -62,13 +103,14 @@ Class bookingAdmin{
 	      $this->table_header($total);
 	        if($results){
 	            foreach($results as $booking){
-	              booking_row_html($booking);
+	              	$this->item($booking);
 	            }
 	        } else {
 	            echo('<tr><td colspan="4"><h3>No booking found.</h3></td></tr>');
 	        }
 	        $this->table_footer($total);
 	}
+
 	function col_full_name(){
 
 	}
@@ -106,12 +148,19 @@ Class bookingAdmin{
 			<thead>
 				<tr>
 					<td id="cb" class="manage-column column-cb check-column"><label class="screen-reader-text" for="cb-select-all-1">Select All</label><input id="cb-select-all-1" type="checkbox"></td>
-					<th scope="col" id="title" class="manage-column column-title column-primary desc">
+					<!-- <th scope="col" id="title" class="manage-column column-title column-primary desc">
 					<span>Full name</span>
 					</th>
 
 					<th scope="col" id="taxonomy-rate" class="manage-column ">Phone</th>
-					<th scope="col" id="taxonomy-room_range" class="manage-column column-taxonomy-room_range">Email</th><th scope="col" id="date" class="manage-column column-date sortable desc"><a href="<?php echo $this->order_url;?>"><span>Date</span><span class="sorting-indicator"></span></a></th>
+					<th scope="col" id="taxonomy-room_range" class="manage-column column-taxonomy-room_range">Email</th><th scope="col" id="date" class="manage-column column-date sortable desc"><a href="<?php echo $this->order_url;?>"><span>Date</span><span class="sorting-indicator"></span></a></th> -->
+
+					<?php foreach($this->colums as $colum){
+						;
+						echo '<th scope="col" id="taxonomy-rate" class="manage-column ">'.$colum['label'].'</th>';
+					}
+					?>
+
 				</tr>
 			</thead>
 
@@ -123,14 +172,19 @@ Class bookingAdmin{
 				<tfoot>
 					<tr>
 					<td class="manage-column column-cb check-column"><label class="screen-reader-text" for="cb-select-all-2">Select All</label><input id="cb-select-all-2" type="checkbox"></td>
-					<th scope="col" class="manage-column column-title column-primary desc"><span>Full name</span></th>
+					<!-- <th scope="col" class="manage-column column-title column-primary desc"><span>Full name</span></th>
 
 					<th scope="col" class="manage-column column-taxonomy-rate">Phone</th>
 					<th scope="col" class="manage-column column-taxonomy-room_range">Email</th>
 					<th scope="col" class="manage-column column-date sortable asc">
-					<a href="<?php echo $this->order_url;?>"><span>Date</span><span class="sorting-indicator"></span></a>
-					</th>
+						<a href="<?php echo $this->order_url;?>"><span>Date</span><span class="sorting-indicator"></span></a>
+					</th> -->
+					<?php foreach($this->colums as $colum){
+						;
+						echo '<th scope="col" id="taxonomy-rate" class="manage-column ">'.$colum['label'].'</th>';
+					} ?>
 					</tr>
+
 				</tfoot>
 
 			</table>
